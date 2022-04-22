7f1ed04f69dd6fa61a63b99a847a02b476769572
Tiffani <TiffaniL95@gmail.com>
2022-04-21 19:59:20 -0700

    wrapped button in anchor tag and adjusted styling to compensate

--- src/topspot.jsx
+++ src/topspot.jsx
@@ -6,7 +6,7 @@ export default props => ({
          <div className={`well m-4 p-4 text-center ${props.name}`}>
             <h4>{props.name}</h4>
             <p>{props.description}</p>
-            <button><a href={`https://maps.google.com/?q=${props.location[0]},${props.location[1]}`} target="_blank">Open in Google Maps</a></button>
+            <a href={`https://maps.google.com/?q=${props.location[0]},${props.location[1]}`} target="_blank"><button>Open in Google Maps</button></a>
          </div>
       )
    }
